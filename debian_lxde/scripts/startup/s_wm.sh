319e36d94eec0c507a633d8ad8c788f7795c411c
Theo.D <duboistheo08@gmail.com>
2022-04-15 11:50:59 +0200

    lxde wip

---
+++ debian_lxde/scripts/startup/s_wm.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+### every exit != 0 fails the script
+#set -e
+
+echo -e "\n------------------ startup of IceWM window manager ------------------"
+
+#----------DEBUG------------ 
+tail -f $STARTUPDIR/*.log $HOME/.vnc/*$DISPLAY.log
+echo "Display=${DISPLAY}"
+
+
+if [ ! -f "$HOME/wm.log" ]; then
+    touch $HOME/wm.log
+fi
+
+
+
+/usr/bin/icewm-session > $HOME/wm.log &
+sleep 1
+#cat $HOME/wm.log
