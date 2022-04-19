594fe9f0607866a77563d991374cfa8318871d51
Kapesteon <charles-flm.gerard@orange.fr>
2022-04-19 15:34:50 +0200

    Added Env variable to docker-compose

--- ubuntu_icewm/scripts/startup/start_vnc.sh
+++ ubuntu_icewm/scripts/startup/start_vnc.sh
@@ -5,8 +5,6 @@ function help (){
     USAGE:
     docker run -it -p <VNCPORT>:5900 <IMAGE> <OPTION>
 
-
-
     OPTIONS:
 
     -i=INTERFACE, --interface   Sets the interface to listen, defaults to all available interfaces
@@ -22,7 +20,6 @@ function createLogs(){
         touch $HOME/.vnc/xvnc.log
     fi
 }
-
 
 INTERFACE=""
 DEBUG=false
@@ -58,8 +55,6 @@ done
 
 if [[ $IS_SECURE == true ]] ; then
     echo "Encryption enabled"
-    ##DEBUG
-    ## -X509Cert ${HOME}/.vnc/cert/${CERT} -X509Key ${HOME}/.vnc/cert/${KEY}
     SECURITY="-SecurityTypes X509Vnc -X509Cert ${HOME}/.vnc/cert/${CERT} -X509Key ${HOME}/.vnc/cert/${KEY}"
   else
     echo "Encryption disabled"
@@ -70,15 +65,10 @@ fi
 VNC_IP=$(hostname -i)
 createLogs
 
-#/usr/bin/Xvnc $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION -rfbport $VNC_PORT -rfbauth "${HOME}/.vnc/passwd" $SECURITY $INTERFACE \
-#  || rm -rfv /tmp/.X*-lock /tmp/.X11-unix &> $HOME/.vnc/xnvc.log || echo "no locks present"
-
 /usr/bin/Xvnc $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION -rfbport $VNC_PORT -rfbauth "${HOME}/.vnc/passwd" $SECURITY $INTERFACE &
 
 echo -e "\n\n------------------ VNC environment started ------------------"
 echo -e "\nVNCSERVER started on DISPLAY= $DISPLAY \n\t=> connect via VNC viewer with $VNC_IP:$VNC_PORT"
-
-
 
 
 #----------DEBUG------------ 
