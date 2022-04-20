e5e9eced7498ce9ff0cce5c287827e5970b482c7
Theo.D <duboistheo08@gmail.com>
2022-04-20 18:04:46 +0200

    new no vnc update

---
+++ novnc/index/app.js
@@ -0,0 +1,35 @@
+//import containers from './containers.json'
+//console.log(containers)
+//var containers = require('./containers.json')
+//containers = fetch('containers.json').then(resp=> resp.json())
+
+fetch("./containers.json")
+.then(response => {
+   return response.json();
+})
+.then(jsondata => {
+  // const template = document.createElement('template')
+
+  // template.innerHTML = `<li v-for="(container, i) in containers" :key="i"><a :href="container" class="machine"><img src="../assets/images/pc.png" alt="PC icon" />poste ${i}</a></li>`
+
+  // On vérifie si le navigateur prend en charge
+  // l'élément HTML template en vérifiant la présence
+  // de l'attribut content pour l'élément template.
+  if ('content' in document.createElement('template')) {
+    // On prépare une ligne pour le tableau
+    const template = document.querySelector('#container')
+
+    jsondata.forEach((container, i) => {
+        const ul = document.querySelector('ul')
+        const clone = document.importNode(template.content, true)
+        const a = clone.querySelector('a')
+        a.href = container
+        const span = clone.querySelector('span')
+        span.textContent = i + 1
+
+        ul.appendChild(clone)
+    })
+  }
+
+});
+
